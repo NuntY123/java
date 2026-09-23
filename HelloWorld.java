605d06e28419f89cc89c3cfaa04d691ce0d94111
Oleg Leyt <zoviefrag@gmail.com>
2026-09-23 21:56:54 +0300

    added time, text in Program

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,10 @@
+import java.time.LocalDate;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello, world!");
+        LocalDate currentDate = LocalDate.now();
+        System.out.println("Текущая дата: " + currentDate);
+        System.out.println("Я буду Java разработчиком!");
     }
 }
